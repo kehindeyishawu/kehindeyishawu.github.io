1e9f66ae8d5ec89898971d0efc857a1c044addd0
Kenny <fkennyf@gmail.com>
2024-07-26 07:35:54 +0100

    completed Hero Section

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
 import {  } from "bootstrap";
 import "../scss/index.scss";
-// import "../css/index.css"
+
+
+// Page Custom Script
+let heroSvgs = document.querySelectorAll("#hero-svgs use");
+// let svgAnimationQue = [...heroSvgs];
+// let heroShowUpAnimation = document.querySelector("#intro-content .show-up");
+// let currentAnimatingSvg;
+let svgCounter = 0;
+
+
+let startHeroSvgAimation= ()=>{
+    heroSvgs.forEach((e)=>{
+        e.classList.contains("appear") ? e.classList.remove("appear") : null;
+    }) 
+    let svgAnimationId = setInterval(() => {
+        heroSvgs[svgCounter].classList.remove('appear')
+        svgCounter++
+        heroSvgs[svgCounter] || (svgCounter = 0)
+        heroSvgs[svgCounter].classList.add('appear')
+        console.log(`${svgCounter} round`)
+    }, 3000)
+    console.log(svgAnimationId)
+}
+
+startHeroSvgAimation()
